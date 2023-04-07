b51aaf55b99c91f1426471d4dfa993af4db3ab26
Moginder <mogiegan@gmail.com>
2023-04-08 00:00:15 +0530

    checking deploy

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,9 +1,24 @@
 import React from "react";
+import mogi from '../Components/assests/mem/mogi.JPG'
+
+import { ContentAnimation,FadeAnimation,CardHomeAnimation } from "../Animation";
 
 const Gallery = () =>{
     return(
         <div>
-            <h1>Gallery</h1>
+
+
+            <div className="py-20">
+            <div  className="relative pl-20 order-2 flex w-1/2 justify-end md:order-1 md:w-1/3">
+          <div  animate={{ rotate: -12 }} className="card-home absolute bg-white" />
+          <div  animate={{ rotate: -3 }} className="card-home absolute bg-secondary-light" />
+          <div  animate={{ rotate: 2 }} className="card-home relative bg-primary-light">
+            <div  className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-xl">
+              <img src={mogi} priority layout="intrinsic" width={700} height={700} alt="Muhamad Rijal" className="relative" />
+            </div>
+          </div>
+        </div>
+        </div>
         </div>
     )
 }
--- src/Components/Ptustb.js
+++ src/Components/Ptustb.js
@@ -18,8 +18,9 @@ import seevaran from './assests/mem/seevaran.jpg';
 import seshan from './assests/mem/seshan.jpg';
 import shivas from './assests/mem/shivas.jpg';
 import srine from './assests/mem/srine.jpg';
-// import tcadit from './assests/mem/tcsdit.jpg';
+import tcadit from './assests/mem/tcadit.jpg';
 import thacha from './assests/mem/thacha.jpg';
+import mogi from './assests/mem/mogi.JPG';
 
 
 const Ptustb = () =>{
@@ -225,6 +226,236 @@ const Ptustb = () =>{
   </div>
 </div>
 </div>
+
+
+<div className="py-6 grid grid-cols-1 md:grid-cols-3">
+    <div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={mogi} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Webmaster</div>
+    <p class="text-gray-700 text-center  text-base">
+     Moginder E
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: </p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 3A<br/>
+    8778434982
+</p>
+  </div>
+</div>
+
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={pravee} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Department Chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Praveene R
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 98975997</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 3B<br/>
+     8438778446 
+</p>
+  </div>
+</div>
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={muriki} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Section chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Krishna Teja Murikipudi
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: </p>
+  </div>
+  <div class="px-6  pb-2">
+    <p> B.Tech, ECE 3A <br/>
+    9652224274
+</p>
+  </div>
+</div>
+</div>
+
+
+
+<div className="py-6 grid grid-cols-1 md:grid-cols-3">
+    <div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={tcadit} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Students Activities Committee chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    T C Adityaa
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 98174133</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 3B<br/>
+    9944581729
+</p>
+  </div>
+</div>
+
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={seevaran} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Section Students Representative</div>
+    <p class="text-gray-700 text-center  text-base">
+    Seevaranjinee
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99122931</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 2B<br/>
+    9629970007
+</p>
+  </div>
+</div>
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={seshan} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Regional students’ activity Committee chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Seshan
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99073238</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p> B.Tech, ECE 2B <br/>
+    6385635310
+</p>
+  </div>
+</div>
+</div>
+
+
+<div className="py-6 grid grid-cols-1 md:grid-cols-3">
+    <div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={nimal} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Regional Students Representative</div>
+    <p class="text-gray-700 text-center  text-base">
+    Nimal Vardhan
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99073299</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 2B<br/>
+    9677747762
+</p>
+  </div>
+</div>
+
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={pranav} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Finance Committee Chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Pranav N
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99197603</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 3B<br/>
+    7540046485
+</p>
+  </div>
+</div>
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={srine} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Program Committee Chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Ra. Srinethe
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 98179516</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p> B.Tech, ECE 3B <br/>
+    6369127917
+</p>
+  </div>
+</div>
+</div>
+
+
+<div className="py-6 grid grid-cols-1 md:grid-cols-3">
+    <div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={kailash} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Publicity Committee Chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Kailash
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99151090</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 2A<br/>
+    6385687148
+</p>
+  </div>
+</div>
+
+
+
+<div class="max-w-[300px] min-w-[300px] hover:shadow-light-neumorphism-l  shadow-light-inner-neumorphism-xl mx-auto rounded-2xl  overflow-hidden shadow-lg">
+  <img class="w-full h-[300px] rounded-tr-2xl rounded-tl-2xl" src={haris} alt="Sunset in the mountains"/>
+  <div class="px-4 py-4">
+    <div class="font-bold text-center text-xl mb-2">Membership Committee Chair</div>
+    <p class="text-gray-700 text-center  text-base">
+    Harshitha
+    </p>
+  </div>
+  <div class="px-6 pb-2">
+    <p className="bg-yellow-400 rounded-xl text-center"> Membership ID: 99197603</p>
+  </div>
+  <div class="px-6  pb-2">
+    <p>B.Tech, ECE 3A<br/>
+     9493188149
+</p>
+  </div>
+</div>
+
+
+
+</div>
+
+
+
+
+
 
 
         </div>
